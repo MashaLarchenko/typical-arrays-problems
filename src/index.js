11fbea72dd3284a95b1c4be64526f61db1c630e6
maria larchenko <mashalarchenko16@gmail.com>
2020-10-01 15:11:52 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  return array.reduce((acc, curr) => {
+    return Math.min(acc, curr)
+  }, []);;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  return array.reduce((acc, curr) => {
+    return Math.max(acc, curr)
+  }, []);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  const avr = array.reduce((acc, curr) => {
+    return +acc + +curr
+  }, []);
+  return avr / array.length
 }
